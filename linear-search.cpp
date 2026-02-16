ba58e2f4db3caf0a2ed0405862800358f632be2d
halima02may <halima.02may@gmail.com>
2026-02-16 22:14:59 +0600

    Add files via upload

---
+++ linear-search.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+using namespace std;
+
+int linearSearch(int arr[], int size, int target) {
+    for(int i = 0; i < size; i++) {
+        if(arr[i] == target) {
+            return i; // index found
+        }
+    }
+    return -1; // not found
+}
+
+int main() {
+    int arr[] = {10, 25, 30, 45, 50};
+    int size = sizeof(arr) / sizeof(arr[0]);
+    int target;
+
+    cout << "Enter number to search: ";
+    cin >> target;
+
+    int result = linearSearch(arr, size, target);
+
+    if(result != -1)
+        cout << "Element found at index: " << result << endl;
+    else
+        cout << "Element not found." << endl;
+
+    return 0;
+}
